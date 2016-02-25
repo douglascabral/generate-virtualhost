a90f10b31f9c1b85fd9c69feb411c37836c47a16
Douglas Cabral <douglas.cabral@fiap.com.br>
2016-02-25 16:37:34 -0300

    Melhorias no codigo fonte

--- generate-virtualhost.sh
+++ generate-virtualhost.sh
@@ -13,27 +13,27 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 echo
 echo "Informe o nome do server (Ex: meusite.com.br ou test.local)"
-read MYSERVER
+read -r MYSERVER
 
 echo
 echo "Informe o caminho dos arquivos (Ex: /var/www/meusite.com.br ou /var/www/test.local)"
-read MYPATH
+read -r MYPATH
 
 sudo mkdir -p "$MYPATH"
-sudo chown -R "$USER:$USER $MYPATH"
+sudo chown -R "$USER":"$USER" "$MYPATH"
 
 #permissão para o diretório web
 sudo chmod -R 755 "$MYPATH"
 
 #cria um arquivo index
-touch "$MYPATH/index.php"
+touch "$MYPATH"/index.php
 
 #phpinfo
-echo '<?php phpinfo();' > "$MYPATH/index.php"
+echo '<?php phpinfo();' > "$MYPATH"/index.php
 
 #cria o virtualhost
 echo "Criando virtualhost para $MYSERVER"
-sudo cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/$MYSERVER.conf
+sudo cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/"$MYSERVER".conf
 
 echo "
 <Directory $MYPATH>
@@ -50,11 +50,11 @@ echo "
 	ErrorLog ${APACHE_LOG_DIR}/error.log
 	CustomLog ${APACHE_LOG_DIR}/access.log combined
 </VirtualHost>
-" | sudo tee /etc/apache2/sites-available/$MYSERVER.conf
+" | sudo tee /etc/apache2/sites-available/"$MYSERVER".conf
 
 #Ativa os novos arquivos de virtual host
 echo "Ativando virtual host de test.local"
-sudo a2ensite $MYSERVER.conf
+sudo a2ensite "$MYSERVER".conf
 
 #restart o apache novamente
 echo "Reiniciando apache2"
